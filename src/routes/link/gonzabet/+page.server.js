ba06f533603bfcc220a92447e4a0f7ad4d60a588
goodoldman887 <goodoldman887@gmail.com>
2025-05-28 19:52:04 +0700

    Add files via upload

--- src/routes/link/gonzabet/+page.server.js
+++ src/routes/link/gonzabet/+page.server.js
@@ -1,18 +1,14 @@
-// src/routes/link/gonzabet/+page.server.js
+// src/routes/link/casinolab/+page.server.js
 import { redirect } from '@sveltejs/kit';
 
-// Отключаем статический prerender, чтобы сборка не пыталась
-// рендерить эту страницу на этапе билда
+// Disable prerendering to ensure server-side redirect
 export const prerender = false;
 
 /** @type {import('./$types').PageServerLoad} */
 export function load() {
-  // Используем 307 Temporary Redirect
+  // Permanent or temporary redirect (307 preserves method)
   throw redirect(
     307,
-    'https://rollingslots45.com/en?stag=121005_6835c0372507a8664d171c4c' +
-    '&clickid=3ju8o9b2ccj87' +
-    '&tracking_link=http://rlsredirect.com/dcd98e345' +
-    '&visit_id=smartlinkrolling#signup'
+    'https://rollingslots45.com/en#signup_continue'
   );
 }
